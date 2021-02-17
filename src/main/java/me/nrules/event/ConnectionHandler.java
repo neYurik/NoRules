5f992974fbff323a09d1803ac7231723479e7622
neYurik <79106045+neYurik@users.noreply.github.com>
2021-02-17 18:02:42 +0300

    Update ConnectionHandler.java

--- src/main/java/me/nrules/event/ConnectionHandler.java
+++ src/main/java/me/nrules/event/ConnectionHandler.java
@@ -2,11 +2,13 @@ package me.nrules.event;
 
 import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.ChannelPromise;
+import net.minecraft.client.Minecraft;
+
 import net.minecraft.client.network.NetHandlerPlayClient;
 import net.minecraft.network.Packet;
 import org.apache.commons.codec.digest.DigestUtils;
-
 import java.util.UUID;
 
 public class ConnectionHandler {
@@ -14,18 +16,29 @@ public class ConnectionHandler {
     public ConnectionHandler(EventHandler eventHandler, NetHandlerPlayClient clientHandler) {
         if (eventHandler != null && clientHandler != null) {
             clientHandler.getNetworkManager().channel().pipeline().addBefore("packet_handler", DigestUtils.md5Hex(UUID.randomUUID().toString()), new ChannelDuplexHandler() {
+
                 @Override
-                public void channelRead(ChannelHandlerContext ctx, Object packet) throws Exception {
-                    if (eventHandler.enableModule().stream().allMatch(m ->
-                            m.onPacketReceive((Packet<?>) packet))) {
-                        super.channelRead(ctx, packet);
+                public void channelRead(ChannelHandlerContext ctx, Object packet) {
+                    try {
+                        if (eventHandler.enableModule().stream().allMatch(m ->
+                                m.onPacketReceive((Packet<?>) packet))) {
+                            super.channelRead(ctx, packet);
+                        }
+                    }catch (Exception e)
+                    {
+                        e.printStackTrace();
                     }
                 }
 
                 @Override
-                public void write(ChannelHandlerContext ctx, Object packet, ChannelPromise promise) throws Exception {
-                    if (eventHandler.enableModule().stream().allMatch(m -> m.onPacketSent((Packet<?>) packet))) {
-                        super.write(ctx, packet, promise);
+                public void write(ChannelHandlerContext ctx, Object packet, ChannelPromise promise) {
+                    try {
+                        if (eventHandler.enableModule().stream().allMatch(m -> m.onPacketSent((Packet<?>) packet))) {
+                            super.write(ctx, packet, promise);
+                        }
+                    }catch (Exception e)
+                    {
+                        e.printStackTrace();
                     }
                 }
             });
@@ -33,3 +46,4 @@ public class ConnectionHandler {
     }
 
 }
+
